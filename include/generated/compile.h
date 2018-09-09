0c8b7ad5ed0c2bcf0793ffb60f088293188ae62e
Phu Nguyen <ngdacphu.khtn@gmail.com>
2018-09-09 06:38:15 -0700

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#4 SMP PREEMPT Wed May 2 17:00:26 CST 2018"
+#define LINUX_COMPILE_BY "lilianhua"
+#define LINUX_COMPILE_HOST "ubuntu70-PowerEdge-R710"
+#define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
